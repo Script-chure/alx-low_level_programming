a9d9eef309670e7cc47c4f81fdeca6aca0852af4
David Yolom Kassa <dfkyolom18@outlook.com>
2022-12-12 17:31:53 -0800

    02. taask 2.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,18 @@
 
 /**
  * main - Prints the alphabet in lower case letters
- *
+ * 
  * Return: always 0(success)
  */
-int main()
+int main(void)
 {
-    int c;
-
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        putchar(c);
-    }
-    putchar('\n');
-    return (0);
+	int c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	
+	return (0);
 }
