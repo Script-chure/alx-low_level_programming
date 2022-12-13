cc086714bcf31b021a714382ebe0b884c9981f14
David Yolom Kassa <dfkyolom18@outlook.com>
2022-12-12 18:00:39 -0800

    Tasking

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case letters
+ * then again in upper case letters
+ * 
+ * Return: always gives the value of 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
