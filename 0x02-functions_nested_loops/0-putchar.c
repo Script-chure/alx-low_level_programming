f225d07c32ea910ade6460a1ad12ea300ef3aabe
David Yolom Kassa <dfkyolom18@outlook.com>
2022-12-14 03:13:31 -0800

    Corrected

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints out the alphabet in lower case
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar('c');
+	}
+	putchar('\n');
+	return (0);
+}
