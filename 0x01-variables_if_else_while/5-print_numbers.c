f22c97c51dddcb6819a4c8a09d3d32d37305bcfd
David Yolom Kassa <dfkyolom18@outlook.com>
2022-12-18 19:35:11 -0800

    Task 5.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers less than 10, from 0.
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
