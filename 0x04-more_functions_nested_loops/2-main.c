d76bcd22a4465c8852973d65a7120c4692c616ed
David Yolom Kassa <dfkyolom18@outlook.com>
2023-01-08 17:07:20 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- checks the code
+ *
+ * Return: always  0.
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024 ));
+	printf("%d\n", mul(-342, 4563));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: ist number
+ * @b: 2nd number
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
