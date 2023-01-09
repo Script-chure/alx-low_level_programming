cf9caa8a2f5630498c20d0a34b31dacf60daf911
David Yolom Kassa <dfkyolom18@outlook.com>
2023-01-08 16:54:34 -0800

    betty prooved Task 0 & 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - checks if character is Uppercesed
+ * _isupper - checks if character is Uppercese
  * @c: character
- * 
+ *
  * Return: 1 if c is upper, else 0.
  */
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-	else 
+	else
 		return (0);
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,13 @@
 /**
  * _isdigit - checks if c is a digit
  * @c: character
- * 
+ *
  * Return: 1 if c is digit, else 0.
  */
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
 		return (1);
-	else 
+	else
 		return (0);
 }
